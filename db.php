f2964ddea76d05fcda7479c9bad7c05c48db818f
Giane <gianellarochavendivengo123@gmail.com>
2026-09-17 23:39:20 -0300

    cambios para deploy

--- db.php
+++ db.php
@@ -3,14 +3,14 @@ require_once __DIR__ . '/config.php';
 
 // Credenciales locales (Laragon) para desarrollo.
 // En producción (DonWeb) usá:
-//   $host = "localhost";
-//   $db   = "l0081231_foto_db";
-//   $user = "l0081231_foto_db";
-//   $pass = "biruRU95re";
-$host = "localhost";
-$db   = "fotografo_db";
-$user = "root";
-$pass = ""; // En Laragon por defecto va vacío
+   $host = "localhost";
+  $db   = "l0081231_foto_db";
+$user = "l0081231_foto_db";
+   $pass = "biruRU95re";
+//$host = "localhost";
+//$db   = "fotografo_db";
+//$user = "root";
+//$pass = ""; // En Laragon por defecto va vacío
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
